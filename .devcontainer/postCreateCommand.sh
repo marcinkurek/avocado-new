5416bf88796303479d59622c061c3f89ce846d0c
Marcin Kurek <marcin.kurek@outlook.com>
2026-02-17 05:08:57 +0000

    Update scripts

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,7 +4,8 @@ npm install
 
 npx nx run backend-api-angular:generate-api
 
-cd libs/database-api
+pushd libs/database-api
     npx prisma migrate dev
+    npx prisma db seed
     npx prisma generate
-cd ../..
+popd
