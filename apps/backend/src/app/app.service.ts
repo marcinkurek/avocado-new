c99f02ebdbcd2bfd1a3d9deeef0240b460c67215
Marcin Kurek <marcin.kurek@outlook.com>
2026-02-17 04:34:59 +0000

    Add seeding

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,8 +1,23 @@
+import 'dotenv/config';
 import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from '@avocado/database-api';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  async getData(): Promise<{ message: string }> {
+    const users = await prisma.user.findMany({
+      include: {
+        posts: true,
+      },
+    });
+
+    return { message: `${users.length}` };
   }
 }
